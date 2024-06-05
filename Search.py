cfb661557c0f28eb307df98e47112afc93d8646c
SayaliYewale <129512714+SayaliYewale@users.noreply.github.com>
2024-06-05 21:55:19 +0530

    Update Search.py

--- Search.py
+++ Search.py
@@ -6,20 +6,20 @@ from mysql.connector import Error
 
 
 class Search(Tk):
-    def __init__(tanmayee):
+    def __init__(self):
         super().__init__()
         f = StringVar()
         g = StringVar()
-        tanmayee.title("Search Book")
-        tanmayee.maxsize(800,500)
-        tanmayee.minsize(800,500)
-        tanmayee.canvas = Canvas(width=800, height=500, bg='gray')
-        tanmayee.canvas.pack()
-        tanmayee.iconbitmap(rFavicon path)
+        self.title("Search Book")
+        self.maxsize(800,500)
+        self.minsize(800,500)
+        self.canvas = Canvas(width=800, height=500, bg='gray')
+        self.canvas.pack()
+        self.iconbitmap(rFavicon path)
         l1=Label(text="Search Library",bg='gray', font=("Courier new",20,'bold')).place(x=290,y=20)
-        l = Label(tanmayee, text="Search By",bg='gray', font=("Courier new", 15, 'bold')).place(x=60, y=96)
+        l = Label(self, text="Search By",bg='gray', font=("Courier new", 15, 'bold')).place(x=60, y=96)
         def insert(data):
-            tanmayee.listTree.delete(*tanmayee.listTree.get_children())
+            tanmayee.listTree.delete(*self.listTree.get_children())
             for row in data:
                 tanmayee.listTree.insert("", 'end', text=row[0], values=(row[1], row[2], row[3]))
 
